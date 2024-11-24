d7f5a1f58e8cfe233e8892f3c4375009f510052f
Simon Prophet <simon.s.prophet@outlook.com>
2024-11-24 14:47:54 +0000

    completed Fetching Data From The API

---
+++ script.js
@@ -0,0 +1,47 @@
+const API_URL =
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=cfcdbf66916818b6c971aa3726a970c2&page=1';
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=cfcdbf66916818b6c971aa3726a970c2&query="';
+
+const form = document.getElementById('form');
+
+const button = document.getElementById('button');
+
+const search = document.getElementById('search');
+
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data.results);
+}
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== '') {
+    getMovies(SEARCH_API + searchTerm);
+
+    search.value = '';
+  } else {
+    window.location.reload();
+  }
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== '') {
+    getMovies(SEARCH_API + searchTerm);
+
+    search.value = '';
+  } else {
+    window.location.reload();
+  }
+});
